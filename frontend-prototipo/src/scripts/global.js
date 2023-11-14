51b9b73bfb1ae46583f9bf157b72b72c920f687e
João Luccas <101368682+devJoaoLuccas@users.noreply.github.com>
2023-11-14 18:39:29 -0300

    fix: fix js

--- frontend-prototipo/src/scripts/global.js
+++ frontend-prototipo/src/scripts/global.js
@@ -5,5 +5,5 @@ document.getElementById("cadastre-se-link").addEventListener("click", function(e
 
 document.getElementById("login-link").addEventListener("click", function(event) {
     event.preventDefault(); 
-    window.location.href = "/frontend-prototipo/src/pages/jogos.html"
+    window.location.href = "/frontend-prototipo/src/pages/index-menu.html"
 }); 
