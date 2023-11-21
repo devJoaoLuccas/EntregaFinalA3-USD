9fbb65a3cc6e1f070aaa8b387942f3a687b6174a
João Luccas <101368682+devJoaoLuccas@users.noreply.github.com>
2023-11-21 08:22:18 -0300

    feat: update routes

--- src/backend/src/routes.js
+++ src/backend/src/routes.js
@@ -1,21 +1,20 @@
 import { Router } from "express";
-
 import { selectUsuarios, selectUsuario, adicionarUsuario, updateUsuario, deleteUsuario } from './controler/usuario.js';
 import { selectJogo, selectJogos, adicionarJogo, updateJogo, deleteJogo } from "./controler/jogos.js";
 import { adicionarPlataforma, deletePlataforma, selectPlataforma, selectPlataformas, updatePlataforma } from "./controler/plataformas.js";
-import { adicionarNotaJogo, deleteNotaJogo, selectNotasJogos, updateNotaJogo } from "./controler/notaJogo.js";
+import { selectNotasJogos } from "./controler/notaJogo.js";
 
 const router = Router();
 
 // Os metódos GET da nossa API, chamando as querys de cada banco. 
 
-router.get('/jogo', selectJogos);
+router.get('/jogo', selectJogo);
 router.get('/usuario', selectUsuario);
 router.get('/plataforma', selectPlataforma);
-router.get('/notasJogos', selectNotasJogos)
+router.get('/usuariosJogos', selectNotasJogos)
 // router.get('/usuarioJogo', selectUsuarioJogo);
 
-router.get('/jogos', selectJogo);
+router.get('/jogos', selectJogos);
 router.get('/usuarios', selectUsuarios);
 router.get('/plataformas', selectPlataformas);
 
@@ -24,20 +23,17 @@ router.get('/plataformas', selectPlataformas);
 router.post('/inserirUsuario', adicionarUsuario);
 router.post('/inserirJogo', adicionarJogo);
 router.post('/inserirPlataforma', adicionarPlataforma);
-router.post('/inserirNotaJogo', adicionarNotaJogo);
 
 // Os metódos PUT da nossa API, chamando as querys de cada banco.
 
 router.put('/updateUsuario', updateUsuario);
 router.put('/updateJogo', updateJogo);
 router.put('/updatePlataforma', updatePlataforma);
-router.put('/updateNotaJogo', updateNotaJogo);
 
 // Os metódos DELETE da nossa API, chamando as querys de cada banco.
 
 router.delete('/deleteUsuario', deleteUsuario);
 router.delete('/deleteJogo', deleteJogo);
 router.delete('/deletePlataforma', deletePlataforma);
-router.delete('/deleteNotaJogo', deleteNotaJogo);
 
 export default router;
