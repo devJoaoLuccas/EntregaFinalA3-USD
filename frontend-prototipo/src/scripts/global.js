f45f6832e267cab9d91b2d7d024587add69d9539
João Luccas <101368682+devJoaoLuccas@users.noreply.github.com>
2023-11-21 08:28:47 -0300

    delete: delete prototipo front-end

--- frontend-prototipo/src/scripts/global.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("cadastre-se-link").addEventListener("click", function(event) {
-    event.preventDefault(); 
-    window.location.href = "/frontend-prototipo/src/pages/telaCadastroUsuario.html"
-}); 
-
-document.getElementById("login-link").addEventListener("click", function(event) {
-    event.preventDefault(); 
-    window.location.href = "/frontend-prototipo/src/pages/index-menu.html"
-}); 
